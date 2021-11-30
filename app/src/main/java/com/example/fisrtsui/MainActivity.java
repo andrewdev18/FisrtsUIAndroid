a70bb6723954d06f4413b9f0742d7e262fa8007f
andrewdev18 <jaime.mora2018@uteq.edu.ec>
2021-11-30 12:39:57 -0500

    Agregados los controles UI y creada la función btEnviar.

--- app/src/main/java/com/example/fisrtsui/MainActivity.java
+++ app/src/main/java/com/example/fisrtsui/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.fisrtsui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btEnviar(View view){
+        //Creamos el Intent
+        Intent intent = new Intent(actMain.this, actBienvenida.class);
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        //Creamos la información a pasar entre actividades - Pares Key-Value
+        Bundle b = new Bundle();
+        b.putString("NOMBRE", txtNombre.getText().toString());
+        //Añadimos la información al intent
+        intent.putExtras(b);
+        // Iniciamos la nueva actividad
+        startActivity(intent);
+
     }
 }
